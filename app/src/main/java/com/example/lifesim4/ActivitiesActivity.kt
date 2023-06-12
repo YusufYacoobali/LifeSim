5acf22bab16a932b7bf027ce73f4200b149a26d9
Yusuf Yacoobali <yyacoobali@gmail.com>
2023-06-12 19:42:18 +0100

    refactor

--- app/src/main/java/com/example/lifesim4/ActivitiesActivity.kt
+++ app/src/main/java/com/example/lifesim4/ActivitiesActivity.kt
@@ -4,14 +4,19 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
+import com.example.lifesim4.models.GameEngine
 
 class ActivitiesActivity : AppCompatActivity() {
+
+    private lateinit var gameEngine: GameEngine
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_activities)
+        gameEngine = GameEngine.getInstance()
 
         val myContract = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
@@ -21,9 +26,15 @@ class ActivitiesActivity : AppCompatActivity() {
         }
 
         val fitnessButton: LinearLayout = findViewById(R.id.fitnessActivity)
-        fitnessButton.setOnClickListener {
-            val intent = Intent(this, FitnessActivity::class.java)
-            myContract.launch(intent)
+        val clickListener = View.OnClickListener { view ->
+            when (view.id) {
+                R.id.fitnessActivity -> {
+                    val intent = Intent(this, FitnessActivity::class.java)
+                    myContract.launch(intent)
+                }
+            }
         }
+
+        fitnessButton.setOnClickListener(clickListener)
     }
 }
--- app/src/main/java/com/example/lifesim4/FitnessActivity.kt
+++ app/src/main/java/com/example/lifesim4/FitnessActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
@@ -20,11 +21,23 @@ class FitnessActivity : AppCompatActivity() {
 
         //handle different buttons
         val gymOption: LinearLayout = findViewById(R.id.gymOption)
-        gymOption.setOnClickListener {
-            gameEngine.goGym()
-            gameEngine.sendMessage("Joined Gym, +1 charm")
+        val swimOption: LinearLayout = findViewById(R.id.swimOption)
+
+        val clickListener = View.OnClickListener { view ->
+            when (view.id) {
+                R.id.gymOption -> {
+                    gameEngine.goGym()
+                    gameEngine.sendMessage("Joined Gym, +1 charm")
+                }
+                R.id.swimOption -> {
+                    gameEngine.sendMessage("You started swimming")
+                }
+            }
             setResult(Activity.RESULT_OK)
             finish()
         }
+
+        gymOption.setOnClickListener(clickListener)
+        swimOption.setOnClickListener(clickListener)
     }
 }
