de0c138e75293437e8480c133e8dae4757015d9a
Yusuf Yacoobali <yyacoobali@gmail.com>
2023-06-13 10:39:37 +0100

    relation ships

---
+++ app/src/main/java/com/example/lifesim4/relations/LoversActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lifesim4.relations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lifesim4.R
+
+class LoversActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lovers)
+    }
+}
